0d8563def3743e406bc1ad8dfdf2a38a8ba4c8dd
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-05-19 10:01:01 -0500

    [UP] Render the grid  when press start button

---
+++ main.js
@@ -0,0 +1,22 @@
+const gameContainer = document.querySelector("#game-container");
+
+const Gameboard = (() => {
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+  const render = () => {
+    let boardHTML = "";
+    gameboard.forEach(
+      (box, index) =>
+        (boardHTML += `<div class="game-box" id="box-${index}">${box}</div>`)
+    );
+    gameContainer.innerHTML = boardHTML;
+    console.log("Rendering");
+  };
+
+  return { render };
+})();
+
+document.querySelector("#btn-start").addEventListener("click", () => {
+  gameContainer.style.display = "grid";
+  Gameboard.render();
+});
