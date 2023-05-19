d67209ce91bf12f43513061623c2b2025e33c46b
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-05-19 10:44:24 -0500

    [UP] Create players system

--- main.js
+++ main.js
@@ -21,10 +21,25 @@ const Gameboard = (() => {
   return { render };
 })();
 
+const createPlayer = (name, signal) => {
+  return { name, signal };
+};
+
+const Game = (() => {
+  let players = [];
+  const start = () => {
+    players = [createPlayer("Dante", "X"), createPlayer("Garu", "O")];
+    console.log(players);
+  };
+
+  return { start };
+})();
+
 document.querySelector("#btn-start").addEventListener("click", () => {
   gameContainer.style.display = "grid";
   setTimeout(function () {
     gameContainer.classList.add("show");
   }, 500);
   Gameboard.render();
+  Game.start();
 });
